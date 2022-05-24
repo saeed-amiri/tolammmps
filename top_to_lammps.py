999da67542ce9ae7fd37375daf88f8797791a37d
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-05-24 13:33:56 +0200

    Update top_to_lammps.py

--- top_to_lammps.py
+++ top_to_lammps.py
@@ -1,4 +1,5 @@
 import os, sys, re
+from pprint import pprint
 import pandas as pd
 
 class DOC:
@@ -8,17 +9,11 @@ class DOC:
 
 class TOP:
     """
-    reading top file
+    reading top file for getting the FLAGS and FORMATS and return a dictionary out of them
     it has 40 FLAG (cards) with different formats in FORTAN style written in the next line
     each flag:
     %FLAG POINTERS                                                                  
     %FORMAT(10I8)                                                                   
-    6702       7    5350       0       0       0       0       0       0       0
-    8468    3062       0       0       0       3       0       0      10       1
-       0       0       0       0       0       0       0       1       8       0
-       0
-
-    The cards are space-parsed in some cases, i.e. names of atoms, mols, residues
     """
     def __init__(self) -> None:
         self.FLAG_list = []
@@ -36,25 +31,64 @@ class TOP:
                     line = line.split('%')[1]
                     if line.startswith('version'): self.get_version(line)
                     if line.startswith('FLAG'): self.get_flag(line)
+                    if line.startswith('FORMAT'): self.get_format(line)
                 if not line: break
-        print(self.FLAG_list)
-    
+        free_dict = [dict() for item in self.FORMAT_list]
+        self.FLAG = dict(zip(self.FLAG_list, free_dict))
+        for i, key in enumerate(self.FLAG): self.FLAG[key]['format'] = self.FORMAT_list[i]
+        
+        del free_dict
+        del self.FLAG_list
+        del self.FORMAT_list
+
     def get_version(self, line) -> str:
-        """geting the version of the AMBER written in top of the file"""
+        """geting the version of the AMBER, written in top of the file"""
         self.version = line.strip()
         del line
 
     def get_flag(self, line) -> list:
-        """getting the FLAGEs and amke list of it"""
+        """getting the FLAGEs and make list of it"""
         flag = line.split('FLAG')[1].strip()
         self.FLAG_list.append(flag)
         del line
 
     def get_format(self, line) -> list:
+        """getting the FORMATs and make list of it"""
         format = line.split('FORMAT')[1].strip()
-        re.findall('\(.*?\)',line)
+        format = re.findall('\((.*?)\)',format)[0]
+        self.FORMAT_list.append(format)
+        del line
 
+class READTOP (TOP):
+    """%FLAG POINTERS                                                                  
+    %FORMAT(10I8)                                                                   
+    6702       7    5350       0       0       0       0       0       0       0
+    8468    3062       0       0       0       3       0       0      10       1
+       0       0       0       0       0       0       0       1       8       0
+       0
+
+    The cards are space-parsed in some cases, i.e. names of atoms, mols, residues
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.read_file()
+
+    def get_data(self)->None:
+        self.mk_modify()
+
+    def mk_modify(self)-> None:
+        """modify the FLAG dict"""
+        self.set_flage()
+
+    def set_flage(self) -> list:
+        """set True and False flag to cards' name"""
+        for key in self.FLAG: self.FLAG[key]['flag']=False
+
+        pprint(self.FLAG)
 if __name__=="__main__":
     TOPFILE = "test3.top"
-    top = TOP()
-    top.read_file()
+    top = READTOP()
+    top.get_data()
+    # top.read_file()
+
+    
