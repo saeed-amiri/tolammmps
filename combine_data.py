ce2fecabe9e9932b25408671b5a7c05bd6568a83
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-06-20 14:00:55 +0200

    Update combine_data.py

--- combine_data.py
+++ combine_data.py
@@ -39,10 +39,10 @@ class Combine:
 
     def set_df_lists(self) -> None:
         """Set lists to append DataFrame in it"""
-        self.l_atoms: list[pd.DataFrame] = []
-        self.l_bonds: list[pd.DataFrame] = []
-        self.l_angles: list[pd.DataFrame] = []
-        self.l_dihedrals: list[pd.DataFrame] = []
+        self.l_atoms: dict[str, pd.DataFrame] = dict()
+        self.l_bonds: dict[str, pd.DataFrame] = dict()
+        self.l_angles: dict[str, pd.DataFrame] = dict()
+        self.l_dihedrals: dict[str, pd.DataFrame] = dict()
         self.l_headers: dict[str, mlmp.Header] = dict()
 
     def get_data(self) -> None:
@@ -53,10 +53,10 @@ class Combine:
             self.l_headers[f] = o
             atoms = mlmp.Body(o.Names, f)
             atoms.read_body()
-            self.l_atoms.append(atoms.Atoms_df)
-            self.l_bonds.append(atoms.Bonds_df)
-            self.l_angles.append(atoms.Angles_df)
-            self.l_dihedrals.append(atoms.Dihedrals_df)
+            self.l_atoms[f] = atoms.Atoms_df
+            self.l_bonds[f] = atoms.Bonds_df
+            self.l_angles[f] = atoms.Angles_df
+            self.l_dihedrals[f] = atoms.Dihedrals_df
 
     def append_atoms(self) -> None:
         """append atoms DataFrame with updataed id and type"""
@@ -67,11 +67,23 @@ class Combine:
         """Update the type and index of the atoms with number of next
         atoms in the next DataFrame
         """
+        # Track the number of atom in each file
+        Natoms: int = 0
+        for i, f in enumerate(self.f_list):
+            if i == 0:
+                # jump the first file, no need for update
+                # save the number of atoms
+                Natoms = self.l_headers[f].NATOMS
+            elif i > 0 and i < len(self.f_list):
+                # Update the the index of the second file
+                self.l_atoms[f]['atom_id'] += Natoms
+                # Add the number of the atoms of the current file
+                Natoms += self.l_headers[f].NATOMS
+            if i+1 > len(self.f_list):
+                break
+
         
-
-
 
 INFILE = sys.argv[1:]
 system = Combine(INFILE)
 system.mk_lmp_df()
-print(next(iter(system.l_headers)))
