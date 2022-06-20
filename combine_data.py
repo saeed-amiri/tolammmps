13ddbf1f95bd9a5ac3fb1710940f3e83ce251696
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-06-20 15:07:55 +0200

    Update combine_data.py

--- combine_data.py
+++ combine_data.py
@@ -5,20 +5,25 @@ import pandas as pd
 import numpy as np
 import read_lmp_data as mlmp  # My lammps
 
+
 class Doc:
     """combining LAMMPS data file to prepare interface
     Input:
         LAMMPS data file[s]
     Output:
         LAMMPS data file
-    
+    [ checking PEP8
+        ~/.local/bin/pycodestyle combine_data.py
+        and typing"
+        ~/.local/bin/mypy combine_data.py
+    ]
     The order of data is based on the input order.
     For now, all the data only stack on top of each other in the
     z-direction.
 
     Usage:
         combine_data.py data1 data2 data3 ...
-    
+
     """
 
 
@@ -35,7 +40,16 @@ class Combine:
         """making DataFrames from all the inputs"""
         self.set_df_lists()
         self.get_data()
-        self.update_atoms_df()
+        self.mk_atoms()
+
+    def mk_atoms(self) -> None:
+        """make atoms DataFrame"""
+        self.Natoms = self.update_atoms_df()
+        self.NATomTyp = self.update_atom_typ()
+        self.Nmols = self.update_atom_mol()
+        self.max_z = self.recenter_atoms()
+        self.Atoms = self.append_atoms()
+        print(self.Natoms, self.NATomTyp)
 
     def set_df_lists(self) -> None:
         """Set lists to append DataFrame in it"""
@@ -58,12 +72,11 @@ class Combine:
             self.l_angles[f] = atoms.Angles_df
             self.l_dihedrals[f] = atoms.Dihedrals_df
 
-    def append_atoms(self) -> None:
+    def append_atoms(self) -> pd.DataFrame:
         """append atoms DataFrame with updataed id and type"""
-        for df in self.l_atoms:
-            print(df)
+        return pd.concat(self.l_atoms)
 
-    def update_atoms_df(self) -> None:
+    def update_atoms_df(self) -> int:
         """Update the type and index of the atoms with number of next
         atoms in the next DataFrame
         """
@@ -81,9 +94,58 @@ class Combine:
                 Natoms += self.l_headers[f].NATOMS
             if i+1 > len(self.f_list):
                 break
+        return Natoms
 
-        
+    def update_atom_typ(self) -> int:
+        """update the number of each type in atoms card"""
+        NATomTyp: int = 0
+        for i, f in enumerate(self.f_list):
+            if i == 0:
+                NATomTyp = self.l_headers[f].NATomTyp
+            elif i > 0 and i < len(self.f_list):
+                self.l_atoms[f]['typ'] += NATomTyp
+                NATomTyp += self.l_headers[f].NATomTyp
+            if i+1 > len(self.f_list):
+                break
+        return NATomTyp
+
+    def update_atom_mol(self) -> int:
+        """update the number of molecules"""
+        Nmols: int = 0
+        for i, f in enumerate(self.f_list):
+            if i == 0:
+                Nmols = self.l_atoms[f]['mol'].max()
+            elif i > 0 and i < len(self.f_list):
+                self.l_atoms[f]['mol'] += Nmols
+                Nmols += self.l_atoms[f]['mol'].max()
+            if i+1 > len(self.f_list):
+                break
+        return Nmols
+
+    def recenter_atoms(self) -> float:
+        """make sure all the atoms are not overlapping after stacking"""
+        max_z: float = 0
+        for i, f in enumerate(self.f_list):
+            self.l_atoms[f] = self.return_to_zero(self.l_atoms[f])
+            if i == 0:
+                max_z = self.l_atoms[f]['z'].max()
+            elif i > 0 and i < len(self.f_list):
+                self.l_atoms[f]['z'] += max_z
+                max_z = self.l_atoms[f]['z'].max()
+            if i+1 > len(self.f_list):
+                break
+        return max_z
+
+    def return_to_zero(self, df: pd.DataFrame) -> pd.DataFrame:
+        """return minimums to zero in all three direction"""
+        axes: list[str] = ['x', 'y', 'z']
+        for ax in axes:
+            min_x: float = df[ax].min()
+            df[ax] -= min_x
+        return df
+
 
 INFILE = sys.argv[1:]
 system = Combine(INFILE)
 system.mk_lmp_df()
+system.Atoms.to_csv('atoms', sep=' ', index=False, header=None)
