cfa9e6c47b004f3a5c6dac4d1e2927f0954b1098
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-06-21 07:33:14 +0200

    Update combine_data.py

--- combine_data.py
+++ combine_data.py
@@ -1,8 +1,5 @@
-import os
 import sys
-import typing
 import pandas as pd
-import numpy as np
 import read_lmp_data as mlmp  # My lammps
 
 
@@ -27,20 +24,22 @@ class Doc:
     """
 
 
-class Combine:
-    """combining LAMMPS data file
+class Atoms:
+    """ update and make a DataFrame for all atoms to be write into file
+    All the inforamtion from atoms shuld come with this class:
+    Natoms, box sizes, ...
     Input:
-        list of the name of input files
+        A list of all the atom DataFrame and Headers
     """
 
-    def __init__(self, file_list: list[str]) -> None:
-        self.f_list = file_list
-
-    def mk_lmp_df(self) -> None:
-        """making DataFrames from all the inputs"""
-        self.set_df_lists()
-        self.get_data()
-        self.mk_atoms()
+    def __init__(self,
+                 l_atoms: dict[str, pd.DataFrame],
+                 headers: dict[str, mlmp.Header],
+                 f_list: list[str]) -> None:
+        self.l_atoms = l_atoms
+        self.l_headers = headers
+        self.f_list = f_list
+        del headers, l_atoms, f_list
 
     def mk_atoms(self) -> None:
         """make atoms DataFrame"""
@@ -50,27 +49,6 @@ class Combine:
         self.max_z = self.recenter_atoms()
         self.Atoms = self.append_atoms()
         print(self.Natoms, self.NATomTyp)
-
-    def set_df_lists(self) -> None:
-        """Set lists to append DataFrame in it"""
-        self.l_atoms: dict[str, pd.DataFrame] = dict()
-        self.l_bonds: dict[str, pd.DataFrame] = dict()
-        self.l_angles: dict[str, pd.DataFrame] = dict()
-        self.l_dihedrals: dict[str, pd.DataFrame] = dict()
-        self.l_headers: dict[str, mlmp.Header] = dict()
-
-    def get_data(self) -> None:
-        """loop over all the files and make several DataFrame"""
-        for f in self.f_list:
-            print(f)
-            o = mlmp.Header(f)
-            self.l_headers[f] = o
-            atoms = mlmp.Body(o.Names, f)
-            atoms.read_body()
-            self.l_atoms[f] = atoms.Atoms_df
-            self.l_bonds[f] = atoms.Bonds_df
-            self.l_angles[f] = atoms.Angles_df
-            self.l_dihedrals[f] = atoms.Dihedrals_df
 
     def append_atoms(self) -> pd.DataFrame:
         """append atoms DataFrame with updataed id and type"""
@@ -145,7 +123,168 @@ class Combine:
         return df
 
 
+class Bonds:
+    """update the bonds DataFrames and return uniq one"""
+
+    def __init__(self,
+                 l_bonds: dict[str, pd.DataFrame],
+                 headers: dict[str, mlmp.Header],
+                 f_list: list[str]) -> None:
+        self.l_bonds = l_bonds
+        self.l_headers = headers
+        self.f_list = f_list
+        del headers, l_bonds, f_list
+
+    def mk_bonds(self) -> None:
+        """make the bond DataFrame"""
+        self.update_atoms_id()
+        self.update_bond_typ()
+        self.Bonds = self.append_bonds()
+
+    def append_bonds(self) -> pd.DataFrame:
+        """append bonds DataFrame with updataed id and type"""
+        return pd.concat(self.l_bonds)
+
+    def update_atoms_id(self) -> None:
+        """Update the atom id (ai, aj)"""
+        # Track the number of atom in each file
+        Natoms: int = 0
+        for i, f in enumerate(self.f_list):
+            if i == 0:
+                Natoms = self.l_headers[f].NATOMS
+            elif i > 0 and i < len(self.f_list):
+                # Update the the index of the second file
+                self.l_bonds[f]['ai'] += Natoms
+                self.l_bonds[f]['aj'] += Natoms
+                # Add the number of the atoms of the current file
+                Natoms += self.l_headers[f].NATOMS
+            if i+1 > len(self.f_list):
+                break
+
+    def update_bond_typ(self) -> int:
+        """update the number of each type in atoms card"""
+        NBondTyp: int = 0
+        for i, f in enumerate(self.f_list):
+            if i == 0:
+                NBondTyp = self.l_headers[f].NBondTyp
+            elif i > 0 and i < len(self.f_list):
+                self.l_bonds[f]['typ'] += NBondTyp
+                NBondTyp += self.l_headers[f].NBondTyp
+            if i+1 > len(self.f_list):
+                break
+        return NBondTyp
+
+class Angles:
+    """update the Angels DataFrames and return uniq one"""
+
+    def __init__(self,
+                 l_angles: dict[str, pd.DataFrame],
+                 headers: dict[str, mlmp.Header],
+                 f_list: list[str]) -> None:
+        self.l_angles = l_angles
+        self.l_headers = headers
+        self.f_list = f_list
+        del headers, l_angles, f_list
+
+    def mk_angles(self) -> None:
+        """make the bond DataFrame"""
+        self.update_atoms_id()
+        self.update_angle_typ()
+        self.Angles = self.append_angles()
+
+    def append_angles(self) -> pd.DataFrame:
+        """append bonds DataFrame with updataed id and type"""
+        return pd.concat(self.l_angles)
+
+    def update_atoms_id(self) -> None:
+        """Update the atom id (ai, aj)"""
+        # Track the number of atom in each file
+        Natoms: int = 0
+        for i, f in enumerate(self.f_list):
+                if i == 0:
+                    Natoms = self.l_headers[f].NATOMS
+                    print(f)
+                elif i > 0 and i < len(self.f_list):
+                    # Update the the index of the second file
+                    try:
+                        self.l_angles[f]['ai'] += Natoms
+                        self.l_angles[f]['aj'] += Natoms
+                        self.l_angles[f]['ak'] += Natoms
+                    except KeyError:
+                        pass
+                    # Add the number of the atoms of the current file
+                    Natoms += self.l_headers[f].NATOMS
+                if i+1 > len(self.f_list):
+                    break
+
+    def update_angle_typ(self) -> int:
+        """update the number of each type in atoms card"""
+        NAngleTyp: int = 0
+        for i, f in enumerate(self.f_list):
+            if i == 0:
+                try:
+                    NAngleTyp = self.l_headers[f].NAngleTyp
+                except KeyError:
+                    pass
+            elif i > 0 and i < len(self.f_list):
+                try: 
+                    self.l_angles[f]['typ'] += NAngleTyp
+                    NAngleTyp += self.l_headers[f].NAngleTyp
+                except KeyError:
+                    pass
+            if i+1 > len(self.f_list):
+                break
+        return NAngleTyp
+
+
+class Combine:
+    """combining LAMMPS data file
+    Input:
+        list of the name of input files
+    """
+
+    def __init__(self, file_list: list[str]) -> None:
+        self.f_list = file_list
+
+    def mk_lmp_df(self) -> None:
+        """making DataFrames from all the inputs"""
+        self.set_df_lists()
+        self.get_data()
+        atoms = Atoms(self.l_atoms, self.l_headers, self.f_list)
+        atoms.mk_atoms()
+        self.Atoms = atoms.Atoms
+        bonds = Bonds(self.l_bonds, self.l_headers, self.f_list)
+        bonds.mk_bonds()
+        self.Bonds = bonds.Bonds
+        angles = Angles(self.l_angles, self.l_headers, self.f_list)
+        angles.mk_angles()
+        self.Angles = angles.Angles
+
+    def set_df_lists(self) -> None:
+        """Set lists to append DataFrame in it"""
+        self.l_atoms: dict[str, pd.DataFrame] = dict()
+        self.l_bonds: dict[str, pd.DataFrame] = dict()
+        self.l_angles: dict[str, pd.DataFrame] = dict()
+        self.l_dihedrals: dict[str, pd.DataFrame] = dict()
+        self.l_headers: dict[str, mlmp.Header] = dict()
+
+    def get_data(self) -> None:
+        """loop over all the files and make several DataFrame"""
+        for f in self.f_list:
+            print(f)
+            o = mlmp.Header(f)
+            self.l_headers[f] = o
+            atoms = mlmp.Body(o.Names, f)
+            atoms.read_body()
+            self.l_atoms[f] = atoms.Atoms_df
+            self.l_bonds[f] = atoms.Bonds_df
+            self.l_angles[f] = atoms.Angles_df
+            self.l_dihedrals[f] = atoms.Dihedrals_df
+
+
 INFILE = sys.argv[1:]
 system = Combine(INFILE)
 system.mk_lmp_df()
-system.Atoms.to_csv('atoms', sep=' ', index=False, header=None)
+system.Atoms.to_csv('atoms', sep='\t', index=False, header=None)
+system.Bonds.to_csv('bonds', sep='\t', index=False, header=None)
+system.Angles.to_csv('angles', sep='\t', index=False, header=None)
