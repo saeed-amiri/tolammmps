4b381f8c145c10c43da219bb52ba59e8e84e2d9f
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-11 09:22:45 +0200

    Add files via upload
    update atoms' type for combination of data

---
+++ update_atom_type.py
@@ -0,0 +1,13 @@
+import typing
+import pandas as pd
+
+class Doc:
+    """update atom type for combination of data
+    input a dict in the following form:
+    {'A': 'decane.data', 'B: 'water.data'}
+    """
+
+class UpdateType:
+    """updata types"""
+    def __init__(self, ) -> None:
+        pass
