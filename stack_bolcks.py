fd6931c6f113cc2db1e2c323c3d072d070b79b85
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-13 14:12:22 +0200

    mamad!

--- stack_bolcks.py
+++ stack_bolcks.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 class Doc:
     """stack atom in Z loop
     first in x and then in y direction"""
@@ -25,49 +24,49 @@ class stack_data:
         col: int      # Column of the block data in struct file
         row: int      # Row of the block data in struct file
         v: list(str)  # List of symbols of the data file
-        item: str     # An item from v
+        item: str     # An item of v
+
+        x_vacume: int = 2  # Space between blocks
 
         x_indent: float = 0.0  # Shift in x column based on previous block
         y_indent: float = 0.0  # Shift in y column based on previous block
         z_indent: float = 0.0  # Shift in z column based on previous block
 
-        x_vacume: int = 2  # Space between blocks
-
-        max_x: float  # max in x column after updating DataFrame
-        max_y: float  # max in y column after updating DataFrame
-        max_z: float  # max in z column after updating DataFrame
-        max_id: int   # max of id column after updatinf DataFrame
-        max_mol: int  # max of mol column after updatinf DataFrame
+        max_x: float = 0 # max in x column after updating DataFrame
+        max_y: float = 0 # max in y column after updating DataFrame
+        max_z: float = 0 # max in z column after updating DataFrame
+        max_id: int = 0   # max of id column after updatinf DataFrame
+        max_mol: int = 0  # max of mol column after updatinf DataFrame
 
         _df: pd.DataFrame  # Temperary dataframe
         Atoms_df: pd.DataFrame  # To return data
-
+        df_list: list[pd.DataFrame] = [] # To append DataFrames
+        df_dict = dict()
         for row, (_, v) in enumerate(self.block.items()):
             for col, item in enumerate(v):
+                _df = self.bs.system[item]['data'].Atoms_df.copy()
                 if row == 0:
-                    if col == 0:
-                        Atoms_df = self.bs.system[item]['data'].Atoms_df
-                        max_x = np.max(Atoms_df['x'])
-                        max_id = np.max(Atoms_df['atom_id'])
-                        max_mol = np.max(Atoms_df['mol'])
-                    else:
-                        _df = self.bs.system[item]['data'].Atoms_df
-                        _df['x'] += max_x
-                        _df['x'] += x_vacume
-                        _df['atom_id'] += max_id
-                        _df['mol'] += max_mol
-                        Atoms_df = pd.concat([Atoms_df, _df])
-                        max_x = np.max(Atoms_df['x'])
-                        max_id = np.max(Atoms_df['atom_id'])
-                        max_mol = np.max(Atoms_df['mol'])
-                        del _df
+                    max_y = np.max(_df['y'])
                 else:
-                    max_y = np.max(Atoms_df['y'])
-                    if col == 0:
-                        _df = self.bs.system[item]['data'].Atoms_df
-                        _df['x'] += max_x
-                        _df['x'] += x_vacume
-                        _df['atom_id'] += max_id
-                        _df['mol'] += max_mol
-                        Atoms_df = pd.concat([Atoms_df, _df])
-                        del _df
+                    _df['y'] += max_y
+                    _df['y'] += x_vacume
+                if col == 0:
+                    df_list.append(_df)
+                    max_x = np.max(_df['x'])
+                    max_id = np.max(_df['atom_id'])
+                    max_mol = np.max(_df['mol'])
+                    del _df
+                else:
+                    _df['x'] += max_x
+                    _df['x'] += x_vacume
+                    _df['atom_id'] += max_id
+                    _df['mol'] += max_mol
+                    max_x = np.max(_df['x'])
+                    max_id = np.max(_df['atom_id'])
+                    max_mol = np.max(_df['mol'])
+                    df_list.append(_df)
+                    del _df
+
+        Atoms_df = pd.concat(df_list, ignore_index=True,  axis=0)
+        # print(Atoms_df)
+        self.Atoms_df = Atoms_df
