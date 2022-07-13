d954b0929a79ee01b8ad42b6f1a964e0c464145a
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-13 17:32:59 +0200

    Update stack_bolcks.py

--- stack_bolcks.py
+++ stack_bolcks.py
@@ -15,29 +15,32 @@ class stack_data:
                  ) -> None:
         self.block = block
         self.bs = bs
-        row_list = self.stack_atoms()
-        self.stack_rows(row_list)
-        del block, bs, row_list
+        self.VACUME: int = 2  # Space between blocks
+        self.stack_atoms()
+        del block, bs
 
-    def stack_atoms(self) -> list[pd.DataFrame]:
-        """stack atoms with updating id of atoms and also mol number"""
+    def stack_atoms(self) -> None:
+        """stack atoms along axises"""
+        row_list = self.stack_x()
+        self.stack_rows(row_list)
+
+    def stack_x(self) -> list[pd.DataFrame]:
+        """stack atoms with updating id of atoms and also mol number
+        this function stack data along x-axis"""
         # Declear variables
         col: int      # Column of the block data in struct file
         row: int      # Row of the block data in struct file
-        v: list(str)  # List of symbols of the data file
+        v: list[str]  # List of symbols of the data file
         item: str     # An item of v
 
-        self.VACUME: int = 2  # Space between blocks
-
         x_indent: float = 0.0  # Shift in x column based on previous block
-
         max_x: float = 0  # max in x column after updating DataFrame
         max_id: int = 0   # max of id column after updatinf DataFrame
         max_mol: int = 0  # max of mol column after updatinf DataFrame
 
-        _df: pd.DataFrame  # Temperary dataframe
         raw_df: pd.DataFrame  # A Dataframe to keep all the row made DF
         row_list: list[pd.DataFrame] = []  # A list to keep the raws DF
+        _df: pd.DataFrame  # Temperary dataframe
         for row, (_, v) in enumerate(self.block.items()):
             df_list: list[pd.DataFrame] = []  # To append DataFrames
             for col, item in enumerate(v):
@@ -64,6 +67,9 @@ class stack_data:
         return row_list
 
     def stack_rows(self, row_list: list[pd.DataFrame]) -> None:
+        """stack atoms with updating id of atoms and also mol number
+        this function stack data from self.stack_atoms along y-axis"""
+        # Declear variables
         max_z: float = 0  # max in z column after updating DataFrame
         z_indent: float = 0  # Shift in z column based on previous block
         x_ave_center: float  # x of center_of_mass of bottom layer
