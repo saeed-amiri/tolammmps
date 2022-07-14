d9830140330d6910cef7a172b7ffa9d2de345d64
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-14 14:30:36 +0200

    Update update_atom_type.py

--- update_atom_type.py
+++ update_atom_type.py
@@ -1,4 +1,5 @@
 import typing
+import numpy as np
 import pandas as pd
 import read_lmp_data as mlmp
 
@@ -15,9 +16,10 @@ class UpdateType:
     def __init__(self, files: dict[str, str]) -> None:
         self.files = files
         del files
-        self.up_files = self.update_atom_type()
+        self.system = self.update_atom_type()
 
     def update_atom_type(self) -> dict[str, dict[str, typing.Any]]:
+        mass_indent: int = 0  # to increase the type of each file
         atom_indent: int = 0  # to increase the type of each file
         bond_indent: int = 0  # to increase the type of each file
         angle_indent: int = 0  # to increase the type of each file
@@ -29,16 +31,22 @@ class UpdateType:
             read_data = mlmp.ReadData(v)
             print(f'{self.__class__.__name__}:\n'
                   f'\tUpdating: {v}\n')
+            read_data.Atoms_df = self.bring_to_zero(read_data.Atoms_df)
             if i == 0:
                 atom_indent += read_data.NAtomTyp
                 bond_indent += read_data.NBondTyp
                 angle_indent += read_data.NAngleTyp
                 dihedral_indent += read_data.NDihedralTyp
+                mass_indent += read_data.NAtomTyp
             else:
                 if read_data.NAtomTyp > 0:
                     read_data.Atoms_df = self._update_type(
                         read_data.Atoms_df, atom_indent)
+                    read_data.Masses_df = self._update_type(
+                        read_data.Masses_df, atom_indent)
+                    print(read_data.Names)
                     atom_indent += read_data.NAtomTyp
+
                 else:
                     exit(f'{self.__class__.__name__}:\n'
                          f'\tERROR:  ZERO atom type in "{v}"')
@@ -60,3 +68,10 @@ class UpdateType:
     def _update_type(self, df: pd.DataFrame, indent: int) -> pd.DataFrame:
         df['typ'] += indent
         return df
+
+    def bring_to_zero(self, atom_df: pd.DataFrame) -> pd.DataFrame:
+        """bring minimum of each block to zero in x, y, and z"""
+        atom_df['x'] -= np.min(atom_df['x'])
+        atom_df['y'] -= np.min(atom_df['y'])
+        atom_df['z'] -= np.min(atom_df['z'])
+        return atom_df
