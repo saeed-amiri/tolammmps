63d15482ea00e28bf48edffb87bc458456c9e30a
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-21 15:21:46 +0200

    Update write_infile.py

--- write_infile.py
+++ write_infile.py
@@ -23,18 +23,35 @@ class WriteIn:
             self.write_info(f)
             self.write_init(f)
             self.write_gpu(f)
+            self.write_include(f)
+            self.write_minimization(f)
 
-    def write_init(self, f:typing.TextIO) -> None:
+    def write_minimization(self, f) -> None:
+        """write minimiation section"""
+        f.write(f"#{'Minimization':.^85}\n")
+        f.write(f'\tminimize 1.0e-3 1.0e-3 1000 10000\n')
+        f.write(f'\twrite_data min.data\n')
+        f.write(f'\treset_timestep 0\n')
+        f.write(f"#{'':`^85}\n")
+
+    def write_include(self, f: typing.TextIO) -> None:
+        """write commands to include or load files"""
+        f.write(f"#{'Include':.^85}\n")
+        f.write(f'\tread_data \n')
+        f.write(f'\tinclude \n')
+        f.write(f"#{'':`^85}\n")
+
+    def write_init(self, f: typing.TextIO) -> None:
         """write initiation of the simulation"""
         f.write(f"#{'Initiation':.^85}\n")
         f.write(f'\tunits \n')
         f.write(f'\ttimestep \n')
         f.write(f'\tatom_style \n')
-        f.write(f'\tkspace_style pppm 0.0001\n')
-        f.write(f'\tspecial_bonds lj/coul 0.0 0.0 0.5\n')
         f.write(f'\tneigh_modify every 1 delay 0 check yes\n')
+        f.write(f'\tspecial_bonds lj/coul 0.0 0.0 0.5\n')
+        f.write(f'\tkspace_style pppm 0.0001\n')
         f.write(f"#{'':`^85}\n")
-    
+
     def write_gpu(self, f: typing.TextIO) -> None:
         f.write(f"#{'GPU specification':.^85}\n")
         f.write(f'\tpackage gpu 1\n')
@@ -54,7 +71,8 @@ class WriteIn:
         today: datetime.date = date.today()
         code: str = f'class: {self.__class__.__name__}'
         i_file: str = f'script: {__file__}'
-        f.write(f"#{'':.^85}\n")
+        f.write(f"#{'':,^85}\n")
+        f.write(f"#{'':`^85}\n")
         f.write(f'#{v:^85}\n')
         f.write(f'#\n')
         f.write(f"#{i_file:^85}\n")
